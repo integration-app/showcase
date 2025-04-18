e2b512cc5b5f9a3b205c6a0d5129afb583f599aa
Bohdan Ptyts <bohdanptyts@gmail.com>
2025-04-18 17:40:35 +0200

    Improve workspace dropdown, add search and organizations grouping

--- src/components/workspace-select/index.tsx
+++ src/components/workspace-select/index.tsx
@@ -1,20 +1,51 @@
+'use client';
 import { useEffect, useState } from 'react';
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 import { useConsoleEntry } from '@/hooks/use-console-entry';
 import { useCurrentWorkspace } from '../providers/workspace-provider';
 
-export const WorkspaceSelect = () => {
+import { Check, ChevronsUpDown } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { cva, VariantProps } from 'class-variance-authority';
+
+const selectVariants = cva('w-full', {
+  variants: {
+    span: {
+      full: 'max-w-full',
+      short: 'max-w-52',
+    },
+  },
+  defaultVariants: {
+    span: 'short',
+  },
+});
+
+export const WorkspaceSelect = ({
+  span,
+}: VariantProps<typeof selectVariants>) => {
+  const [open, setOpen] = useState(false);
+
   const {
     workspaces = [],
+    orgs = [],
     isError: workspacesError,
     isLoading: workspaceLoading,
+    workspacesMap,
   } = useConsoleEntry();
   const { saveWorkspace, workspace: currentWorkspace } = useCurrentWorkspace();
 
@@ -26,39 +57,79 @@ export const WorkspaceSelect = () => {
     }
   }, [currentWorkspace?.id]);
 
-  if (!currentWorkspace) {
-    return null;
-  }
+  // if (!currentWorkspace) {
+  //   return null;
+  // }
 
   return (
-    <Select
-      value={workspaceId}
-      onValueChange={(newId) => {
-        const workspace = workspaces.find(({ id }) => id === newId);
-        saveWorkspace(workspace!);
-      }}
-      disabled={workspacesError || workspaceLoading}
-    >
-      <SelectTrigger
-        id='workspace'
-        className='max-w-50 min-w-12 w-full'
-        loading={workspaceLoading}
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant='outline'
+          role='combobox'
+          // todo: aria-expanded
+          className={cn('justify-between', selectVariants({ span }))}
+          disabled={workspacesError || workspaceLoading}
+        >
+          <span className='truncate'>
+            {workspaceId
+              ? workspacesMap[workspaceId]?.name
+              : 'Select workspace...'}
+          </span>
+          <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className={cn('p-0', selectVariants({ span }))}
+        align='start'
       >
-        <SelectValue placeholder='Select a workspace' />
-      </SelectTrigger>
-      <SelectContent>
-        {workspaces
-          .sort((left, right) => {
-            const leftUpdatedAt = new Date(left.updatedAt);
-            const rightUpdatedAt = new Date(right.updatedAt);
-            return rightUpdatedAt.getTime() - leftUpdatedAt.getTime();
-          })
-          .map((ws) => (
-            <SelectItem key={ws.id} value={ws.id}>
-              {ws.name}
-            </SelectItem>
-          ))}
-      </SelectContent>
-    </Select>
+        <Command>
+          <CommandInput placeholder='Search workspace...' />
+          <CommandList>
+            <CommandEmpty>No workspaces found.</CommandEmpty>
+            {orgs.sort(updatedAtPredicate).map((org) => {
+              return (
+                <CommandGroup key={org.id} heading={org.name}>
+                  {workspaces
+                    .filter((ws) => ws.orgId === org.id)
+                    .sort(updatedAtPredicate)
+                    .map((ws) => (
+                      <CommandItem
+                        key={ws.id}
+                        keywords={[ws.name, org.name]}
+                        value={ws.id}
+                        onSelect={(newId) => {
+                          const workspace = workspacesMap[newId];
+                          if (workspace) {
+                            saveWorkspace(workspace);
+                            setOpen(false);
+                          }
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            'mr-2 h-4 w-4',
+                            workspaceId === ws.id ? 'opacity-100' : 'opacity-0',
+                          )}
+                        />
+                        {ws.name}
+                      </CommandItem>
+                    ))}
+                </CommandGroup>
+              );
+            })}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
+};
+
+const updatedAtPredicate = (
+  left: { updatedAt: string },
+  right: { updatedAt: string },
+) => {
+  const leftUpdatedAt = new Date(left.updatedAt);
+  const rightUpdatedAt = new Date(right.updatedAt);
+  return rightUpdatedAt.getTime() - leftUpdatedAt.getTime();
 };
--- src/hooks/use-console-entry.ts
+++ src/hooks/use-console-entry.ts
@@ -1,10 +1,21 @@
 import useSWR from 'swr';
 
 import { personalAccessTokenAuthFetcher } from '@/lib/fetch-utils';
-import { ConsoleEntry } from '@/types/console-entry';
+import { ConsoleEntry, Organization } from '@/types/console-entry';
 import { useConsoleAuth } from '@/components/providers/console-auth-provider';
+import { useMemo } from 'react';
+
+type WorkspaceMap = Record<string, ConsoleEntry['workspace']>;
+type OrganizationMap = Record<string, Organization>;
 
 export function useConsoleEntry(): Partial<ConsoleEntry> & {
+  workspaces?: Array<
+    ConsoleEntry['workspaces'][number] & { org?: Organization }
+  >;
+
+  workspacesMap: WorkspaceMap;
+  orgsMap: OrganizationMap;
+
   isLoading: boolean;
   isError: boolean;
 } {
@@ -19,8 +30,44 @@ export function useConsoleEntry(): Partial<ConsoleEntry> & {
     },
   );
 
+  const workspacesMap = useMemo(() => {
+    return (
+      data?.workspaces.reduce<Record<string, ConsoleEntry['workspace']>>(
+        (acc, workspace) => {
+          acc[workspace.id] = workspace;
+          return acc;
+        },
+        {},
+      ) || {}
+    );
+  }, [data?.workspaces]);
+
+  const orgsMap = useMemo(() => {
+    return (
+      data?.orgs.reduce<Record<string, Organization>>((acc, org) => {
+        acc[org.id] = org;
+        return acc;
+      }, {}) || {}
+    );
+  }, [data?.orgs]);
+
+  const workspacesWithOrgs = useMemo(() => {
+    return (
+      data?.workspaces.map<
+        ConsoleEntry['workspaces'][number] & { org?: Organization }
+      >((workspace) => {
+        return { ...workspace, org: orgsMap[workspace.orgId] };
+      }) || []
+    );
+  }, [data?.workspaces, orgsMap]);
+
   return {
-    workspaces: data?.workspaces,
+    workspaces: workspacesWithOrgs,
+    orgs: data?.orgs,
+
+    workspacesMap,
+    orgsMap,
+
     isLoading,
     isError: !!error,
   };
