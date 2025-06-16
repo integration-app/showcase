4322fda8ced07e60969063a2e3dd7057f8dc98fb
Jacek Smolak <jacek.smolak@gmail.com>
2025-06-16 15:27:51 +0200

    SDK-31: fix URL generation for component in GH
    The problem was that in dev mode, import.meta.url, which is used to
    construct the URL to GitHub component, gets passed as is.
    For production build, the URI from import.meta.url is escaped.
    To cover for both cases, the path used in this component is
    normalized (decoded), so that both dev and prod will have the
    same value (dev will not change, but it will change for prod),
    and only then it is escaped, producing the same result for both envs.

--- src/components/open-gh-button.tsx
+++ src/components/open-gh-button.tsx
@@ -18,6 +18,15 @@ export const OpenGhButton = ({
   className?: string;
 }) => {
   const filePath = metaUrl.split('/src')[1];
+  const fullUrl = `${REPO_BASE_URL}${filePath}`;
+
+  // When the app is built, any URLs passed are escaped by default (prod env).
+  // To have it encoded (but on a non-encoded URI), unescape is done, to
+  // normalize the URI for both cases. The dev will not change, as it will
+  // not have anything to escape, therefore will produce the same result.
+  const normalizedUrl = decodeURI(fullUrl);
+
+  const urlToOpen = encodeURI(normalizedUrl);
 
   return (
     <Tooltip>
@@ -29,7 +38,7 @@ export const OpenGhButton = ({
           disabled={!filePath}
           className={className}
         >
-          <a href={encodeURI(`${REPO_BASE_URL}${filePath}`)} target='_blank'>
+          <a href={urlToOpen} target='_blank'>
             <GithubIcon />
           </a>
         </Button>
